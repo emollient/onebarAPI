ff9dd411967e9e0ee6b297da9218e7f3e4f4deaa
Susan Lunn <slgems@gmail.com>
2014-03-28 18:13:46 -0400

    favorites stubs

--- restapi/views/favorites.py
+++ restapi/views/favorites.py
@@ -1,2 +1,26 @@
 from restapi.services import Favorites
-fr
+from restapi.validators import JSON, ValidFields
+from restapi.models import DBSession
+
+
+
+
+@Favorites.get(renderer='json')
+def getFavorites(request):
+    pass
+
+@Favorites.get(renderer='json')
+def orderFavorites(request):
+    pass
+
+@Favorites.post(validators=[ValidJSON, ValidFields('rank','service_id')])
+def addFavorites(request):
+    pass
+
+@Favorites.delete()
+def removeFavorites(request):
+    """
+    does it's own thing
+    """
+    pass
+
