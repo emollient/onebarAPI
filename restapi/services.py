a0f62e9f3ce6eb9c127327d306fde27b2305315d
Susan Lunn <slgems@gmail.com>
2014-03-31 22:47:50 -0400

    services changes to not conflict in views

--- restapi/services.py
+++ restapi/services.py
@@ -1,10 +1,10 @@
 from cornice import Service
 
 
-Favorites = Service("favorites", "/favorites", description = "user favorites api")
+Faves = Service("favorites", "/favorites", description = "user favorites api")
 
 
-CSH_Services = Service("csh_favorites", "/csh_favorites", description = "csh services api")
+cshServices = Service("csh_favorites", "/csh_favorites", description = "csh services api")
 
 
 
